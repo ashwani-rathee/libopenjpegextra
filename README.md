8449369a739cac1aff724dc178eb4676bcdf4477
Ashwani Rathee <ab669522@gmail.com>
2022-10-09 23:29:55 +0545

    testing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # libopenjpegextra
 
-make
-make install
+sudo make
+sudo make install
 gcc test.c -o test.o -I/usr/local/include/openjpeg-2.5 -L/usr/local/lib -lopenjp2 -lopenjpegextra
+./test.o
--- test.c
+++ test.c
@@ -11,16 +11,16 @@ int main()
     opj_image_t *image;
 
     // Decoding success
-    image = decode("sample1.jp2");
+    // image = decode("sample1.jp2");
+    image = decode("sample.j2k");
+    // // opj_image_t to rgb buffer data
+    // rgb buf = imagetorgbbuffer(image);
 
-    // opj_image_t to rgb buffer data
-    rgb buf = imagetorgbbuffer(image);
-
-    // rgb buffer to opj_image_t format
-    opj_image_t *image1 = rgbbuffertoimage(image, buf); // I use image to get the original settings
+    // // rgb buffer to opj_image_t format
+    // opj_image_t *image1 = rgbbuffertoimage(image, buf); // I use image to get the original settings
 
     // opj_image_t image to jp2 file
-    encode("sample.jp2", image1);
+    encode("sample1.jp2", image);
 
     return 0;
 }
